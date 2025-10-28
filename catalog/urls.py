00f3c508ba2d7b951e913c2913ac3a766241bab8
locadm <nigger@gmail.com>
2025-10-28 13:09:35 +0700

    Guide Part 6: Creating a url books

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,14 @@
 from . import views
 from django.urls import path
+from django.urls import re_path
 
 urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
+urlpatterns += [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,19 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
@@ -20,3 +32,22 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+def book_detail_view(request,pk):
+    try:
+        book_id=Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    #book_id=get_object_or_404(Book, pk=pk)
+
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context={'book':book_id,}
+    )
+
